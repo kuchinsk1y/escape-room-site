515ad58436e0b6ac6514cdedb2c5156c9cb66275
kuchinsk1y <98710097+kuchinsk1y@users.noreply.github.com>
2025-07-31 20:06:41 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import React, { FC } from "react"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-
-import CatalogPage from "./pages/CatalogPage"
-import QuestDetailsPage from "./pages/QuestDetailsPage"
-import ContactsPage from "./pages/ContactsPage"
-import NotFoundPage from "./pages/NotFoundPage"
+import { BrowserRouter } from "react-router-dom"
+import { AppRouter } from "./routes/AppRouter"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 
@@ -13,16 +9,12 @@ const App: FC = () => (
     <Header />
     <main className="bg-gradient-to-b from-[#1c1c1c] via-[#222] to-[#1c1c1c] min-h-screen">
       <div className="max-w-[1550px] mx-auto">
-        <Routes>
-          <Route path="/" element={<CatalogPage />} />
-          <Route path="/detailed-quest/:id" element={<QuestDetailsPage />} />
-          <Route path="/contacts" element={<ContactsPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
+        <AppRouter />
       </div>
     </main>
     <Footer />
   </BrowserRouter>
+
 )
 
 export default App
