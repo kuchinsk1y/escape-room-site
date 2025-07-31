423d2993547e382ce6c5204620f201988478ce1a
kuchinsk1y <98710097+kuchinsk1y@users.noreply.github.com>
2025-07-31 20:16:08 +0300

    Update ContactsPage.tsx

--- src/pages/ContactsPage.tsx
+++ src/pages/ContactsPage.tsx
@@ -1,12 +1,14 @@
-import React, { FC } from "react"
+import React from "react"
 
-const ContactsPage: FC = () => {
+const ContactsPage = () => {
   return (
-    <div className="p-4 max-w-[1200px] mx-auto">
+    <main className="p-4 max-w-[1200px] mx-auto">
       <h1 className="text-2xl font-bold mb-4">Контакти</h1>
       <address className="not-italic mb-4">м. Київ, вул. Прикладна, 1</address>
-      <div className="w-full h-96 bg-gray-200 flex items-center justify-center">карта</div>
-    </div>
+      <div className="w-full h-96 bg-gray-200 flex items-center justify-center">
+        карта
+      </div>
+    </main>
   )
 }
 
